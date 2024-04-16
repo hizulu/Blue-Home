d858212cb153466c6047c48424120d9fecef146d
AdrianUBU <aag1057@alu.ubu.es>
2024-04-16 13:51:10 +0200

    Added Animations Ruddy

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CharacterController : MonoBehaviour
+{
+    [Header("Configuracion de Movimiento")]
+    [SerializeField]private float velocidad;
+    [SerializeField] private float recoveryTime = 0f;
+
+    [Header("Colision")]
+    private Rigidbody2D _rig;
+    private Animator _anim;
+
+    //llama al rig y lo guarda para realizar colisiones
+    private void Awake()
+    {
+        _rig = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        //esto es para cuando establezcamos un punto donde queramos que spawnee
+
+        //puntoInicial = GameObject.Find("PuntoInicial").transform.position;  
+        //gameObject.transform.position = puntoInicial;
+    }
+
+    private void FixedUpdate()      //siempre irá a la misma velocidad
+    {
+        Debug.Log("se mueve");
+        Movimiento();
+    }
+
+    private void Movimiento()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (recoveryTime > 0)
+        {
+            recoveryTime -= Time.deltaTime;
+            return;
+        }
+
+        Vector2 direccion = new Vector2(horizontal, vertical); //Los personajes no deben moverse más rápido en diagonal
+
+        if (direccion.magnitude > 1)
+        {
+            direccion = direccion.normalized;
+        }
+
+        _rig.velocity = direccion * velocidad;
+
+        if (horizontal != 0 || vertical != 0) //esto cambia las animaciones
+        {
+            _anim.SetFloat("CaminaHorz", horizontal);
+            _anim.SetFloat("CaminaVert", vertical);
+
+            _anim.SetFloat("CaminaHorz", horizontal);
+            _anim.SetFloat("CaminaVert", vertical);
+
+            _anim.SetBool("Camina", true);
+        }
+        else
+        {
+            _anim.SetBool("Camina", false);
+        }
+    }
+}
