44ef6ddb748413307bac50b0950668ba8c17dd17
AdrianUBU <118912014+AdrianUBU@users.noreply.github.com>
2024-05-18 11:10:04 +0200

    Arreglos del texto del telefono y dialogos

--- Assets/Scripts/Elements/Objetos dialogo.cs
+++ Assets/Scripts/Elements/Objetos dialogo.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class Objetosdialogo : MonoBehaviour, IInteractuable
 {
     [Header("Elementos del dialogo")]
-    [SerializeField] public Collider2D colliderObjeto;
+    [SerializeField]
+    public Collider2D colliderObjeto;
+
     [SerializeField] public GameObject marcaOpcionInteraccion;
     [SerializeField] GameObject panelDialogoObjeto;
     [SerializeField] public Image imagenIntermitente;
@@ -14,79 +16,87 @@ public class Objetosdialogo : MonoBehaviour, IInteractuable
     [SerializeField] public Image[] imagenesDialogoObjeto;
 
     [Header("Variables activas por objeto")]
-    [SerializeField, TextArea(4, 6)] public string[] lineasDialogoObjeto;
+    [SerializeField, TextArea(4, 6)]
+    public string[] lineasDialogoObjeto;
 
     public float tiempoEntreLetras = 0.2f;
     private bool dialogoActivo;
     private int indexLinea;
 
+
     public void Interactuar()
     {
-        if (!dialogoActivo)
+        if (!dialogoActivo) // Si no hay dialogo activo y no se ha leido el dialogo
         {
+            dialogoActivo = true; // Establecer dialogoActivo a true cuando se inicia el di logo
             ActivarDialogo();
+        }
+        else if (dialogoActivo &&
+                 textoDialogoObjeto.text == lineasDialogoObjeto[indexLinea]) // Si ya hay un dialogo activo
+        {
+            if (lineasDialogoObjeto[indexLinea].Contains(""))
+            {
+                textoDialogoObjeto.text = "";
+                FinalizarDialogo();
+            }
+            else
+            {
+                SiguienteLineaDialogo();
+            }
         }
         else
         {
-            // Para cuando se reactiva el dialogo
             StopAllCoroutines();
             textoDialogoObjeto.text = lineasDialogoObjeto[indexLinea];
-            foreach (var imagen in imagenesDialogoObjeto)
-            {
-                imagen.gameObject.SetActive(false);
-            }
-            imagenIntermitente.gameObject.SetActive(true);
-
-            indexLinea++; // Pasar al siguiente diálogo
-            if (indexLinea >= lineasDialogoObjeto.Length)
-            {
-                // Si no hay más diálogos, desactivar el panel de diálogo y reanudar el tiempo
-                panelDialogoObjeto.SetActive(false);
-                marcaOpcionInteraccion.SetActive(true);
-                imagenIntermitente.gameObject.SetActive(false);
-                dialogoActivo = false;
-                Time.timeScale = 1f;
-            }
+            imagenesDialogoObjeto[0].gameObject.SetActive(true);
         }
     }
 
-    private void ActivarDialogo() // Activa el diálogo, pausa el tiempo y muestra las líneas de diálogo
+    private void ActivarDialogo() // Activa el dialogo, pone el tiempo en pausa y muestra las lineas de dialogo
     {
-        dialogoActivo = true;
         panelDialogoObjeto.SetActive(true);
-        marcaOpcionInteraccion.SetActive(false);
-        indexLinea = UnityEngine.Random.Range(0, lineasDialogoObjeto.Length);
+        imagenIntermitente.gameObject.SetActive(false); // Desactivar la imagen intermitente al iniciar el di logo
+        indexLinea = 0;
         Time.timeScale = 0f;
-
-        // Resetear el texto del diálogo
-        textoDialogoObjeto.text = string.Empty;
-
-        // Activar todas las imágenes de diálogo al iniciar un nuevo diálogo
-        foreach (var imagen in imagenesDialogoObjeto)
-        {
-            imagen.gameObject.SetActive(true);
-        }
-
         StartCoroutine(MostrarLineasDialogo());
     }
 
 
-    private void DesactivarDialogo() // Desactiva el diálogo y reanuda el tiempo
+    private void SiguienteLineaDialogo() // Muestra la siguiente linea de dialogo
+    {
+        textoDialogoObjeto.text = ""; // Coco es un juego de palabras (Lo hace ver mas pro los dialogos)
+        indexLinea++;
+        if (indexLinea < lineasDialogoObjeto.Length)
+        {
+            StopCoroutine(MostrarLineasDialogo());
+            imagenIntermitente.gameObject.SetActive(false);
+            StartCoroutine(MostrarLineasDialogo());
+        }
+        else // Si ya no hay mas lineas de dialogo, se desactiva el panel de dialogo y se reactiva la opcion de interaccion
+        {
+            FinalizarDialogo();
+        }
+    }
+
+    void FinalizarDialogo()
     {
         panelDialogoObjeto.SetActive(false);
-        marcaOpcionInteraccion.SetActive(true);
+        //marcaOpcionInteraccion.SetActive(true);
         dialogoActivo = false;
+
         Time.timeScale = 1f;
+        imagenesDialogoObjeto[0].gameObject.SetActive(true);
+        imagenIntermitente.gameObject.SetActive(false); // Desactivar la imagen intermitente al finalizar el di logo   
     }
 
     private IEnumerator MostrarLineasDialogo()
     {
         // Variable para controlar el tiempo de cambio de imagen
-        float tiempoCambioImagen = Random.Range(0.2f, 1f); 
+        float tiempoCambioImagen = Random.Range(0.2f, 1f);
         int indexImagen = 0;
 
-        // Ciclo para mostrar las líneas de diálogo letra por letra
-        foreach (char letra in lineasDialogoObjeto[indexLinea].ToCharArray())
+        // Ciclo para mostrar las l neas de di logo letra por letra
+        foreach (char letra in lineasDialogoObjeto[indexLinea])
         {
             // Mostrar la letra actual
             textoDialogoObjeto.text += letra;
@@ -95,23 +105,18 @@ public class Objetosdialogo : MonoBehaviour, IInteractuable
             // Si es hora de cambiar de imagen
             if (tiempoCambioImagen <= 0)
             {
-                // Cambiar a la siguiente imagen de diálogo
+                // Cambiar a la siguiente imagen de di logo
                 imagenesDialogoObjeto[indexImagen].gameObject.SetActive(false);
                 indexImagen = (indexImagen + 1) % imagenesDialogoObjeto.Length;
                 imagenesDialogoObjeto[indexImagen].gameObject.SetActive(true);
 
                 tiempoCambioImagen = Random.Range(0.2f, 1f);
             }
+
             yield return new WaitForSecondsRealtime(tiempoEntreLetras);
         }
 
-        // Una vez que el diálogo ha terminado de escribirse, desactivar todas las imágenes de diálogo
-        foreach (var imagen in imagenesDialogoObjeto)
-        {
-            imagen.gameObject.SetActive(false);
-        }
 
         imagenIntermitente.gameObject.SetActive(true);
     }
-
 }
--- Assets/Scripts/Elements/Telefono.cs
+++ Assets/Scripts/Elements/Telefono.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class Telefono : MonoBehaviour, IInteractuable
 {
     [Header("Elementos del dialogo")]
-    [SerializeField] public Collider2D colliderTelefono;
+    [SerializeField]
+    public Collider2D colliderTelefono;
+
     [SerializeField] public GameObject marcaOpcionInteraccion;
     [SerializeField] GameObject panelDialogoTelefono;
     [SerializeField] public Image[] imagenesDialogoObjeto;
@@ -17,32 +19,37 @@ public class Telefono : MonoBehaviour, IInteractuable
     private int indexLinea;
 
     [SerializeField] public float tiempoEntreLetras = 0.2f;
-    [SerializeField, TextArea(4, 6)] public string[] lineasDialogoTelefono; // Array de lineas de dialogo, cada elemento es una linea
+
+    [SerializeField, TextArea(4, 6)]
+    public string[] lineasDialogoTelefono; // Array de lineas de dialogo, cada elemento es una linea
 
     [Header("Variables activas por telefono")]
-    [SerializeField] public GameObject[] objetosActivadosPorTelefono; // Array de objetos que se activan al interactuar con el telefono
+    [SerializeField]
+    public GameObject[] objetosActivadosPorTelefono; // Array de objetos que se activan al interactuar con el telefono
 
     //asegurar que no lea dos veces el mismo dialogo
     public bool dialogoTelefonoLeido = false;
-    
+
 
     private void Start()
     {
-        // Desactivar el panel de diálogo y la imagen intermitente al iniciar
+        // Desactivar el panel de di logo y la imagen intermitente al iniciar
         panelDialogoTelefono.SetActive(false);
         imagenIntermitente.gameObject.SetActive(false);
     }
 
     public void Interactuar()
     {
-        if (dialogoTelefonoLeido) return; // No hacer nada si el diálogo ya fue leído
+        // dialogoTelefonoLeido = Dialogo.Instance.Empezar(panelDialogoTelefono.GetComponent<Image>(), lineasDialogoTelefono, tiempoEntreLetras, imagenesDialogoObjeto, dialogoTelefonoLeido, objetosActivadosPorTelefono, true); Fue una buena idea hasta que me canse de intentarlo
+        if (dialogoTelefonoLeido) return; // No hacer nada si el di logo ya fue le do
 
         if (!dialogoActivo && !dialogoTelefonoLeido) // Si no hay dialogo activo y no se ha leido el dialogo
         {
-            dialogoActivo = true; // Establecer dialogoActivo a true cuando se inicia el diálogo
+            dialogoActivo = true; // Establecer dialogoActivo a true cuando se inicia el di logo
             ActivarDialogo();
         }
-        else if (dialogoActivo && textoDialogoTelefono.text == lineasDialogoTelefono[indexLinea]) // Si ya hay un dialogo activo
+        else if (dialogoActivo &&
+                 textoDialogoTelefono.text == lineasDialogoTelefono[indexLinea]) // Si ya hay un dialogo activo
         {
             SiguienteLineaDialogo();
         }
@@ -50,13 +57,14 @@ public class Telefono : MonoBehaviour, IInteractuable
         {
             StopAllCoroutines();
             textoDialogoTelefono.text = lineasDialogoTelefono[indexLinea];
+            imagenesDialogoObjeto[0].gameObject.SetActive(true);
         }
     }
 
     private void ActivarDialogo() // Activa el dialogo, pone el tiempo en pausa y muestra las lineas de dialogo
     {
         panelDialogoTelefono.SetActive(true);
-        imagenIntermitente.gameObject.SetActive(false); // Desactivar la imagen intermitente al iniciar el diálogo
+        imagenIntermitente.gameObject.SetActive(false); // Desactivar la imagen intermitente al iniciar el di logo
         indexLinea = 0;
         Time.timeScale = 0f;
         StartCoroutine(MostrarLineasDialogo());
@@ -65,18 +73,20 @@ public class Telefono : MonoBehaviour, IInteractuable
 
     private void SiguienteLineaDialogo() // Muestra la siguiente linea de dialogo
     {
+        textoDialogoTelefono.text = ""; // Coco es un juego de palabras (Lo hace ver mas pro los dialogos)
         indexLinea++;
         if (indexLinea < lineasDialogoTelefono.Length)
         {
-            StopAllCoroutines();
+            StopCoroutine(MostrarLineasDialogo());
             imagenIntermitente.gameObject.SetActive(false);
             StartCoroutine(MostrarLineasDialogo());
         }
         else // Si ya no hay mas lineas de dialogo, se desactiva el panel de dialogo y se reactiva la opcion de interaccion
         {
-            FinalizarDialogo();        
+            FinalizarDialogo();
         }
     }
+
     void FinalizarDialogo()
     {
         panelDialogoTelefono.SetActive(false);
@@ -87,7 +97,8 @@ public class Telefono : MonoBehaviour, IInteractuable
         //Activar todos los objetos que nosotros queremos que se activen al interactuar con el telefono
         ActivarObjetosActivadosPorTelefono();
         Time.timeScale = 1f;
-        imagenIntermitente.gameObject.SetActive(false); // Desactivar la imagen intermitente al finalizar el diálogo   
+        imagenesDialogoObjeto[0].gameObject.SetActive(true);
+        imagenIntermitente.gameObject.SetActive(false); // Desactivar la imagen intermitente al finalizar el di logo   
     }
 
     private IEnumerator MostrarLineasDialogo()
@@ -96,8 +107,8 @@ public class Telefono : MonoBehaviour, IInteractuable
         float tiempoCambioImagen = Random.Range(0.2f, 1f);
         int indexImagen = 0;
 
-        // Ciclo para mostrar las líneas de diálogo letra por letra
-        foreach (char letra in lineasDialogoTelefono[indexLinea].ToCharArray())
+        // Ciclo para mostrar las lineas de dialogo letra por letra
+        foreach (char letra in lineasDialogoTelefono[indexLinea])
         {
             // Mostrar la letra actual
             textoDialogoTelefono.text += letra;
@@ -106,21 +117,17 @@ public class Telefono : MonoBehaviour, IInteractuable
             // Si es hora de cambiar de imagen
             if (tiempoCambioImagen <= 0)
             {
-                // Cambiar a la siguiente imagen de diálogo
+                // Cambiar a la siguiente imagen de di logo
                 imagenesDialogoObjeto[indexImagen].gameObject.SetActive(false);
                 indexImagen = (indexImagen + 1) % imagenesDialogoObjeto.Length;
                 imagenesDialogoObjeto[indexImagen].gameObject.SetActive(true);
 
                 tiempoCambioImagen = Random.Range(0.2f, 1f);
             }
+
             yield return new WaitForSecondsRealtime(tiempoEntreLetras);
         }
 
-        // Una vez que el diálogo ha terminado de escribirse, desactivar todas las imágenes de diálogo
-        foreach (var imagen in imagenesDialogoObjeto)
-        {
-            imagen.gameObject.SetActive(false);
-        }
 
         imagenIntermitente.gameObject.SetActive(true);
     }
@@ -132,5 +139,4 @@ public class Telefono : MonoBehaviour, IInteractuable
             objeto.SetActive(true);
         }
     }
-
 }
